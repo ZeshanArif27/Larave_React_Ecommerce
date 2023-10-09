6aee75a599f8defa2c541078a4f6b24c06c62757
ZeshanArif27 <zeshanarif.official@gmail.com>
2023-10-08 23:11:12 -0700

    Updated-7Oct2023

--- app/Http/Controllers/Products_Controller.php
+++ app/Http/Controllers/Products_Controller.php
@@ -11,13 +11,45 @@ use function PHPUnit\Framework\isEmpty;
 class Products_Controller extends Controller
 {
     public function showProds($cat_id){
-        $products = prod::select('cat_id', 'title', 'dimensions', 'features', 'stock', 'price')->where('cat_id', $cat_id)->get();
+        $products = prod::select('cat_id', 'title', 'dimensions', 'image', 'features', 'stock', 'price')->where('cat_id', $cat_id)->get();
         
         if ($products->isEmpty()) {
             return response()->json(['message' => 'No products found for Cat_id: ' . $cat_id], 404);
         }
-        return $products;
-    }
+    
+        // Create an array to store the results
+        $result = [];
+    
+        foreach ($products as $product) {
+            $productData = [
+                'cat_id' => $product->cat_id,
+                'title' => $product->title,
+                'dimensions' => $product->dimensions,
+                'image' => $product->image ? base64_encode($product->image) : null, // Encode image if it exists
+                'features' => $product->features,
+                'stock' => $product->stock,
+                'price' => $product->price,
+            ];
+            $result[] = $productData;
+        }
+    
+        return $result;
+}
+    
+
+    /**The Working One  **/
+
+    // public function showProds($cat_id){
+    //     $products = prod::select('cat_id', 'title', 'dimensions', 'features', 'stock', 'price')->where('cat_id', $cat_id)->get();
+        
+    //     if ($products->isEmpty()) {
+    //         return response()->json(['message' => 'No products found for Cat_id: ' . $cat_id], 404);
+    //     }
+    //     return $products;
+    // }
+
+    /**  **/
+
     // public function get_Products(){
     //     $product = Products::select('id','cat_id','title')->get();
     //     return $product;
